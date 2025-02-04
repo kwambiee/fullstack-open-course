c9577a81960a7163e4c592c473dfb61250fdbf05
Joy Kwamboka <joy@nathandigital.com>
2025-02-04 10:08:00 +0300

    Refactor App and MostVoted components to optimize state management and improve performance

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback, useMemo } from 'react'
 import { anecdotes } from './anecdote'
 import Proverbs from './Proverbs';
 import MostVoted from './MostVoted';
@@ -6,44 +6,53 @@ import './App.css';
 
 
 const App = () => {
-
-  
   const [anecdoteList, setAnecdoteList] = useState(anecdotes);
   const [anecdote, setAnecdote] = useState(anecdotes[0]);
 
   const getRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     return anecdotes[randomIndex];
-  }
-
-  
-  
-  const showNextAnecdote = () => { 
-    setAnecdote(getRandomAnecdote());
-  }
-
-  // Handle voting logic
-  const handleVote = (id) => { 
-    const updatedAnecdoteList = anecdoteList.map(item =>
-      item.id === id ? { ...item, vote: item.vote + 1 } : item
-    );
-
-    setAnecdoteList(updatedAnecdoteList);
-
-    const updatedAnecdote = updatedAnecdoteList.find(item => item.id === id);
-    setAnecdote(updatedAnecdote);
- 
   };
 
+  const showNextAnecdote = () => {
+    setAnecdote(getRandomAnecdote());
+  };
+
+  // Handle voting logic
+  const handleVote = useCallback(
+    (id) => {
+      const updatedAnecdoteList = anecdoteList.map((item) =>
+        item.id === id ? { ...item, vote: item.vote + 1 } : item
+      );
+
+      setAnecdoteList(updatedAnecdoteList);
+
+      const updatedAnecdote = updatedAnecdoteList.find(
+        (item) => item.id === id
+      );
+      setAnecdote(updatedAnecdote);
+    },
+    [anecdoteList]
+  );
+
+
+  const mostVotedAnecdote = useMemo(() => {
+    return anecdoteList.reduce((max, anecdote) =>
+    anecdote.vote > max.vote ? anecdote : max, anecdoteList[0])
+  }, [anecdoteList]);
 
   return (
-    <div className='container flex flex-col items-center h-screen mx-auto max-w-2xl'>
-      <Proverbs anecdote={anecdote} handleVote={handleVote} showNextAnecdote={showNextAnecdote} />
-      
+    <div className="container flex flex-col items-center h-screen mx-auto max-w-2xl">
+      <Proverbs
+        anecdote={anecdote}
+        handleVote={handleVote}
+        showNextAnecdote={showNextAnecdote}
+      />
+
       {/* Show most Voted Anecdote */}
-      < MostVoted  anecdotes={anecdoteList} />
+      < MostVoted  content={mostVotedAnecdote.content} vote={mostVotedAnecdote.vote} />
     </div>
-  )
+  );
 }
 
 export default App
--- part1/anecdote/src/MostVoted.jsx
+++ part1/anecdote/src/MostVoted.jsx
@@ -1,19 +1,19 @@
-const MostVoted = ({ anecdotes }) => { 
+const MostVoted = ({ content, vote }) => { 
 
-    const mostVoted = anecdotes.reduce((max, anecdote) => 
-        anecdote.vote > max.vote ? anecdote : max, anecdotes[0]
-    );
+    // const mostVoted = anecdotes.reduce((max, anecdote) => 
+    //     anecdote.vote > max.vote ? anecdote : max, anecdotes[0]
+    // );
     
     return (
       <div className="m-4 p-4 w-full">
         <h1 className="text-3xl font-bold ">Anecdote with most votes</h1>
-        {mostVoted.vote > 0 ? (
+        {content && vote  ? (
           <>
             <p className=" font-light italic px-4 mt-6">
-              "{mostVoted.content}"
+              "{content}"
             </p>
             <p className=" pl-32 font-medium italic">
-              Has {mostVoted.vote} votes
+              Has {vote} votes
             </p>
           </>
         ) : (
